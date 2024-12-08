6e725b4e6859628f53fa4d26815480fd7cd0e55d
muntasir-mahmud-abdullah <muntasir525@gmail.com>
2024-12-08 10:08:56 +0600

    All equipment page server side done day 1

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
-const express = require("express");
 require("dotenv").config();
+const express = require("express");
 const cors = require("cors");
 const app = express();
 const port = process.env.PORT || 5000;
@@ -22,7 +22,7 @@ const client = new MongoClient(uri, {
 async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
-    await client.connect();
+    // await client.connect();
     const equipmentsCollection = client
       .db("equipmentsDB")
       .collection("equipments");
@@ -93,7 +93,7 @@ async function run() {
     });
 
     // Send a ping to confirm a successful connection
-    await client.db("admin").command({ ping: 1 });
+    // await client.db("admin").command({ ping: 1 });
     console.log(
       "Pinged your deployment. You successfully connected to MongoDB!"
     );
